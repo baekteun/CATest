f0d935a195693bfc89a60ed7c1297d3292658d49
baegteun <baegteun@gmail.com>
2021-12-29 19:08:36 +0900

    [Feat] usecase create BeerListUseCase

--- Target/Source/Domain/UseCase/BeerListUseCase.swift
+++ Target/Source/Domain/UseCase/BeerListUseCase.swift
@@ -2,5 +2,14 @@ import RxSwift
 import Moya
 
 protocol BeerListUseCase{
-    func execute(page: Int) -> Single<Response>
+    func execute(page: Int) -> Single<[Beer]>
+}
+
+final class DefaultBeerListUseCase: BeerListUseCase{
+    
+    @Inject private var beerListRepository: BeerListRepository
+    
+    func execute(page: Int) -> Single<[Beer]> {
+        return beerListRepository.getBeerList(page: page)
+    }
 }
