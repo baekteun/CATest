cd752d2876ac88dc607aad6f1fef5cbb5507fe93
baegteun <baegteun@gmail.com>
2021-12-29 13:13:57 +0900

    [Feat] Inject dependency injection setup

---
+++ Target/Source/App/Container/ContainerDependencies.swift
@@ -0,0 +1,32 @@
+//
+//  ContainerDependencies.swift
+//  CATest
+//
+//  Created by 최형우 on 2021/12/29.
+//  Copyright © 2021 baegteun. All rights reserved.
+//
+
+import Swinject
+import SwinjectAutoregistration
+
+extension Container{
+    func registerDependencies(){
+        registerVC()
+        registerReactor()
+        registerRepository()
+        registerUsecase()
+    }
+    
+    private func registerVC(){
+        
+    }
+    private func registerReactor(){
+        
+    }
+    private func registerRepository(){
+        
+    }
+    private func registerUsecase(){
+        
+    }
+}
