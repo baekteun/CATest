ffa00c6d5f859fad0b7561ea18bba96b06085f92
baegteun <baegteun@gmail.com>
2021-12-30 14:17:26 +0900

    📝 :: UseCase create DefaultSearchBeerUseCase

---
+++ Target/Source/Data/UseCases/DefaultSearchBeerUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultSearchBeerUseCase.swift
+//  CATest
+//
+//  Created by 최형우 on 2021/12/30.
+//  Copyright © 2021 baegteun. All rights reserved.
+//
+
+import RxSwift
+
+final class DefaultSearchBeerUseCase: SearchBeerUseCase{
+    
+    @Inject private var searchBeerRepository: SearchBeerRepository
+    
+    func execute(id: Int) -> Single<Beer> {
+        return searchBeerRepository.getBeer(id: id)
+    }
+}
