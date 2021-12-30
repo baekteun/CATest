f232d09539b12b2ff4051ef33e9b2a37e279e57a
baegteun <baegteun@gmail.com>
2021-12-30 13:18:03 +0900

    [Feat] BeerSearch UI
    create beerSearch UI setup

--- Target/Source/Presentation/Scene/BeerSearch/VC/BeerSearchVC.swift
+++ Target/Source/Presentation/Scene/BeerSearch/VC/BeerSearchVC.swift
@@ -7,10 +7,39 @@
 //
 
 import UIKit
+import SnapKit
 
 final class BeerSearchVC: baseVC<BeerSearchReactor>{
     // MARK: - Properties
     private let beerView = BeerView()
     private let searchController = UISearchController(searchResultsController: nil)
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
+    
+    // MARK: - UI
+    override func addView() {
+        [beerView, activityIndicator].forEach{ view.addSubview($0) }
+    }
+    override func setLayout() {
+        beerView.snp.makeConstraints {
+            $0.top.equalTo(view.layoutMarginsGuide)
+            $0.size.equalToSuperview()
+        }
+        activityIndicator.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    override func configureVC() {
+        setupNavigation()
+        setupSearch()
+    }
+    private func setupNavigation(){
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "Search By ID"
+        self.navigationItem.accessibilityLabel = "Search By ID"
+    }
+    private func setupSearch(){
+        self.navigationItem.searchController = searchController
+        searchController.searchBar.keyboardType = .numberPad
+    }
     
 }
