3b1420e744b05c695c2b50ef937907ebd5c8adb5
baegteun <baegteun@gmail.com>
2021-12-30 13:57:16 +0900

    [Feat] Repository
    create SearchBeerRepository

--- Target/Source/Domain/Interfaces/Repositories/BeerListRepository.swift
+++ Target/Source/Domain/Interfaces/Repositories/BeerListRepository.swift
@@ -1,6 +1,5 @@
 
 import RxSwift
-import Moya
 
 protocol BeerListRepository{
     func getBeerList(page: Int) -> Single<[Beer]>
---
+++ Target/Source/Domain/Interfaces/Repositories/SearchBeerRepository.swift
@@ -0,0 +1,13 @@
+//
+//  SearchBeerRepository.swift
+//  CATest
+//
+//  Created by 최형우 on 2021/12/30.
+//  Copyright © 2021 baegteun. All rights reserved.
+//
+
+import RxSwift
+
+protocol SearchBeerRepository{
+    func getBeer(id: Int) -> Single<Beer>
+}
