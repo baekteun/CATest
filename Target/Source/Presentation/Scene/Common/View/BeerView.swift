b57f2b006c4421e87c5229faa91a5c985a914764
baegteun <baegteun@gmail.com>
2021-12-30 12:23:15 +0900

    [Feat] BeerView
    create BeerView

---
+++ Target/Source/Presentation/Scene/Common/View/BeerView.swift
@@ -0,0 +1,56 @@
+//
+//  BeerView.swift
+//  CATest
+//
+//  Created by 최형우 on 2021/12/30.
+//  Copyright © 2021 baegteun. All rights reserved.
+//
+
+import UIKit
+import Then
+import SnapKit
+import Kingfisher
+
+final class BeerView: UIView{
+    // MARK: - Properties
+    private let beerImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let beerIDLabel = UILabel().then {
+        $0.textColor = .orange
+        $0.font = .systemFont(ofSize: UIFont.smallSystemFontSize)
+    }
+    
+    private let beerNameLabel = UILabel().then {
+        $0.textColor = .black
+    }
+    
+    private let beerDescriptionLabel = UILabel().then {
+        $0.textColor = .lightGray
+        $0.numberOfLines = 0
+    }
+    
+    private let beerStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .center
+        $0.spacing = 10
+    }
+    
+    // MARK: - Init
+    override func draw(_ rect: CGRect) {
+        beerStack.addArrangeSubviews([beerImageView, beerIDLabel, beerNameLabel, beerDescriptionLabel])
+        addSubview(beerStack)
+        beerStack.snp.makeConstraints {
+            $0.top.leading.bottom.trailing.equalToSuperview().inset(5)
+        }
+    }
+    
+    // MARK: - Method
+    func bind(beer: Beer){
+        beerImageView.kf.setImage(with: URL(string: beer.imageUrl) ?? .none)
+        beerIDLabel.text = "\(beer.id)"
+        beerNameLabel.text = beer.name
+        beerDescriptionLabel.text = beer.description
+    }
+}
