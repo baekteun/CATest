d5ee1ee56f63a8b273e8a2fab1c093db1dae9aa4
baegteun <baegteun@gmail.com>
2021-12-31 15:41:53 +0900

    📑 :: Flow creatae randomBeerFlow

--- Target/Source/Flow/Flows/RandomBeer/RandomBeerFlow.swift
+++ Target/Source/Flow/Flows/RandomBeer/RandomBeerFlow.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2021 baegteun. All rights reserved.
 //
 
-import Foundation
+import RxFlow
+import RxRelay
+
+struct RandomBeerStepper: Stepper{
+    let steps: PublishRelay<Step> = .init()
+    
+    var initialStep: Step{
+        return CAStep.randomBeerIsRequired
+    }
+}
+
+final class RandomBeerFlow: Flow{
+    // MARK: - Properties
+    var root: Presentable{
+        return self.rootVC
+    }
+    @Inject private var vc: RandomBeerVC
+    @Inject private var reactor: RandomBeerReactor
+    @Inject var stepper: RandomBeerStepper
+    private let rootVC = UINavigationController()
+    
+    // MARK: - Init
+    
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+    
+    // MARK: - Navigate
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step.asCAStep else { return .none }
+        
+        switch step{
+        case .randomBeerIsRequired:
+            return coordinateToRandomBeer()
+        default:
+            return .none
+        }
+    }
+}
+
+// MARK: - Method
+private extension RandomBeerFlow{
+    func coordinateToRandomBeer() -> FlowContributors{
+        self.rootVC.setViewControllers([vc], animated: true)
+        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: reactor))
+    }
+}
